9dde2e47eeaa80a0931840593ea2ec5b41aa8ea6
Dhans <sgdhans@gmail.com>
2021-12-12 16:42:30 +0530

    utlis and loghandlers using winston

---
+++ src/interfaces/Yamishi.ts
@@ -0,0 +1,57 @@
+import { Client, ColorResolvable, WebhookClient } from "discord.js";
+
+import { Command } from "./commands/Command";
+import { Context } from "./contexts/Context";
+/**
+ * Model used to pass around Yami's Discord instance with additional
+ * configurations attached.
+ */
+
+export interface Yamishi extends Client {
+  debugHook: WebhookClient;
+  currencyHook: WebhookClient;
+  configs: {
+    token: string;
+    dbToken: string;
+    whUrl: string;
+    currencyUrl: string;
+    nasaKey: string;
+    ownerId: string;
+    love: string;
+    yes: string;
+    no: string;
+    think: string;
+    version: string;
+    id: string;
+    homeGuild: string;
+    topGG: string;
+    voteChannel: string;
+    habiticaKey: string;
+    orbitKey: string;
+  };
+  colours: {
+    default: ColorResolvable;
+    success: ColorResolvable;
+    warning: ColorResolvable;
+    error: ColorResolvable;
+  };
+  commands: Command[];
+  responses: {
+    missingGuild: string[];
+    invalidCommand: string[];
+    noPermission: string[];
+    ownerOnly: string[];
+    noModSelf: string[];
+    noModBecca: string[];
+    noEmoteSelf: string[];
+    noEmoteBecca: string[];
+  };
+  contexts: Context[];
+  sass: {
+    greeting: string[];
+    amirite: string[];
+    sorry: string[];
+    selfthanks: string[];
+    beccathanks: string[];
+  };
+}
