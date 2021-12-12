19cd4e1afafe54a88bafda7a100c85cf3722fa3a
Dhans <sgdhans@gmail.com>
2021-12-12 16:42:06 +0530

    index.ts modified

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 import { Client } from "discord.js";
 import { IntentOptions } from "./config/IntentOptions";
+import { Yamishi } from "./interfaces/Yamishi";
 /**
  * This is the entry point for Yami's process. This will log the boot process,
  * call the necessary helpers to prepare Yami, and then log in to Discord.
